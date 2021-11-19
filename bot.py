3df723fa8ba8ff26cf7aa8f6301e8dd33edd40dc
mellgit <daniilhorn8@gmail.com>
2021-11-19 19:48:39 +0300

    test working version

--- bot.py
+++ bot.py
@@ -2,3 +2,18 @@ import telebot
 import conf
 
 bot = telebot.TeleBot(conf.KEY)
+
+@bot.message_handler(content_types=['text'])
+def get_messages(message):
+
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+
+bot.polling(none_stop=True, interval=0)
