4be6599d0fcbfc18a9fb01a01148c02dc5bfe63e
mellgit <daniilhorn8@gmail.com>
2021-11-19 20:41:02 +0300

    add system CRUD for db

--- bot.py
+++ bot.py
@@ -6,14 +6,25 @@ bot = telebot.TeleBot(conf.KEY)
 @bot.message_handler(content_types=['text'])
 def get_messages(message):
 
-    if message.text == "Привет":
-        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    if message.text == "/create":
+        bot.send_message(message.from_user.id, f"create")
 
+    elif message.text == "/read":
+        bot.send_message(message.from_user.id, "read")
+
+    elif message.text == "/update":
+        bot.send_message(message.from_user.id, "update")
+
+    elif message.text == "/delete":
+        bot.send_message(message.from_user.id, "delete")
+
+    
     elif message.text == "/help":
-        bot.send_message(message.from_user.id, "Напиши привет")
+        bot.send_message(message.from_user.id,
+                         "/create, /read, /update, /delete")
 
     else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+        bot.send_message(message.from_user.id, "use /help.")
 
 
 bot.polling(none_stop=True, interval=0)
